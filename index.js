06effa77667509251ce5af297ebd4e9aad5d5548
BhArgAvNaiD <bhargav.naidu.mbn.18@gmail.com>
2023-01-26 20:45:17 +0530

    full website updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
-const list = document.querySelector('#list');
+const list = document.querySelector('.toggle');
 
 hamburger.addEventListener('click', () => {
-    list.classList.toggle('toggle');
+    list.classList.toggle('block');
     list.classList.toggle('toggle');
 });
