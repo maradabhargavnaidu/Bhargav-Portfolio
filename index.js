21c4f7afebc4d196555da8d599f214cdd5cfffb6
BhArgAvNaiD <bhargav.naidu.mbn.18@gmail.com>
2023-02-07 18:04:19 +0530

    updated footer

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const hamburger = document.querySelector('.hamburger');
 const list = document.querySelector('.toggle');
 
 hamburger.addEventListener('click', () => {
+    list.style.transition="transform 5s ease-in-out";
     list.classList.toggle('block');
     list.classList.toggle('toggle');
+    
 });
 
 list.addEventListener('click',() => {
