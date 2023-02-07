6e7b8afed0a4e992c039b997e5a467a781163ef9
BhArgAvNaiD <bhargav.naidu.mbn.18@gmail.com>
2023-02-07 12:05:08 +0530

    updated nav scrolling

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ hamburger.addEventListener('click', () => {
     list.classList.toggle('block');
     list.classList.toggle('toggle');
 });
+
+list.addEventListener('click',() => {
+    list.classList.toggle('toggle');
+    list.classList.toggle('block');
+})
