d5a620f24bb8c9a0dad984d1f0094ec7fff2e2b9
BhArgAvNaiD <bhargav.naidu.mbn.18@gmail.com>
2023-02-26 09:48:22 +0530

    some updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,9 @@
 const Footer = () => {
+    const github = <a href="https://github.com/BhArgAvNaiD"><i class="fa fa-github" aria-hidden="true"></i></a>
+    const linkedin = <a href="https://www.linkedin.com/in/bhargav-naidu"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+    const instagram = <a href="https://instagram.com/maradabhargavnaidu?igshid=YzgyMTM2MGM="><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    const Mail = <a href="mailto:bhargav.naidu.mbn.18@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+
     return(
         <footer className="footer-bg">
             <div className="footer-content">
@@ -6,10 +11,10 @@ const Footer = () => {
                 <h6 style={{fontSize:'20px'}}>STAY CONNECTED</h6>
                 {/* <!-----------------------Footer-Links--------------------------> */}
                 <div class="links">
-                    <a href="https://github.com/BhArgAvNaiD"><i class="fa fa-github" aria-hidden="true"></i></a>
-                    <a href="https://www.linkedin.com/in/bhargav-naidu"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                    <a href="https://instagram.com/maradabhargavnaidu?igshid=YzgyMTM2MGM="><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                    <a href="mailto:bhargav.naidu.mbn.18@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>
+                    {github}
+                    {linkedin}
+                    {instagram}
+                    {Mail}
                 </div><br/>
                 <p style={{fontSize:'15px'}}>bhargavnaidu &copy; 2023</p>
             </div>
--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -5,7 +5,7 @@ const Skills = () => {
             <div className="contents-container">
                 <div className="tech-skills">
                     <h1 className="center" style={{color:'#00337C'}}>Technical Skills</h1>
-                    <h3 className="item">HTML5<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" height="40px" width="40px" alt='html'/></h3>
+                    <h3 className="item">HTML5<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"  height="40px" width="40px" alt='html'/></h3>
                     <h3 className="item">CSS3<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" height="40px" width="40px" alt='css'/></h3>
                     <h3 className="item">JAVASCRIPT
                         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" height="40px" width="40px" alt='javascript'/>
