d67889e9c834202d5fea08c8a2def4242eff937d
jayakrishnaaa <bhargav.naidu.mbn.18@gmail.com>
2023-08-06 18:55:52 +0530

    padding update

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,7 +8,7 @@ const About = () => {
     >
       <div className="container mx-auto md:px-20  md:text-5xl text-4xl text-center px-5">
         <p className="text-blue-600">About</p>
-        <div className="space-y-5 py-20 text-left">
+        <div className="space-y-5 py-8 text-left">
           <p className="text-green-700">Who is Bhargav ?</p>
           <p className="md:text-5xl text-left text-4xl">
             I am a full-stack developer currently pursuing my second year of
