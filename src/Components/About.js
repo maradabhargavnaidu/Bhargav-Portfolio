8a98498e5dda93760cd8b6517c2b9922dbdf0b4b
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2023-08-25 23:21:37 +0530

    2nd to 3rd

--- src/Components/About.js
+++ src/Components/About.js
@@ -11,7 +11,7 @@ const About = () => {
         <div className="space-y-5 py-8 text-left">
           <p className="text-green-700">Who is Bhargav ?</p>
           <p className="md:text-5xl text-left text-4xl">
-            I am a full-stack developer currently pursuing my second year of
+            I am a full-stack developer, currently pursuing my 3RD year of
             B.Tech at Malla Reddy Engineering College. I have a strong interest
             in coding.
           </p>
