1254e069dbbd7bcee15becdb582e62f3fc319785
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2024-05-22 23:16:19 +0530

    update in projects

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
-import Projects from "./components/Projects";
 import { BackgroundGradientDemo } from "./components/BackgroundGradientDemo";
 function App() {
   return (
