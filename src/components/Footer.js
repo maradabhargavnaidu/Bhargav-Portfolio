b2e2cf795cb2142377986cd427f23bbb330f0374
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2025-09-21 13:00:56 +0530

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="p-4 mt-12 font-semibold border-t-2 border-gray-500 flex gap-4 flex-col md:flex-row">
-      <div>Copyright 2025 MBN</div>
+      <div className="text-purple-500">Copyright 2025 MBN</div>
 
       <a href="https://www.linkedin.com/in/bhargav-naidu/">LinkedIn</a>
       <a href="https://github.com/maradabhargavnaidu">GitHub</a>
